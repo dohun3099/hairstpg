5d41c3e72388df0fa5b1e2d588febc706dfd6380
dohun3099 <127476037+dohun3099@users.noreply.github.com>
2023-10-16 23:43:41 +0900

    Update ex3-3.sh

--- src/Assignment03/ex3-3.sh
+++ src/Assignment03/ex3-3.sh
@@ -7,7 +7,7 @@ read -p "몸무게(kg)를 입력하세요: " weight
 read -p "신장(m)를 입력하세요: " height
 
 # BMI 계산
-bmi=$(echo "scale=2; $weight / ($height * $height)" | bc)
+bmi=$(echo "scale=2; $weight / ($height * $height)" | bc -l)
 
 # BMI 결과 출력
 echo "계산된 BMI: $bmi"

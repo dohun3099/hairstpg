2cd2d68307d8d567207a97c899ca8f7b111df8a0
dohun3099 <127476037+dohun3099@users.noreply.github.com>
2023-10-16 23:09:53 +0900

    Update ex3-4.sh

--- src/Assignment03/ex3-4.sh
+++ src/Assignment03/ex3-4.sh
@@ -7,9 +7,9 @@ read -p "리눅스가 재밌나요? (yes / no): " response
 response_lower=$(echo "$response" | tr '[:upper:]' '[:lower:]')
 
 # 입력값이 유효한지 확인
-if [[ "$response_lower" == "yes" ]]; then
+if [[ "$response_lower" == *yes* || "$response_lower" == "y" ]]; then
   echo "멋져요! 리눅스는 정말 재밌습니다."
-elif [[ "$response_lower" == "no" ]]; then
+elif [[ "$response_lower" == *no* || "$response_lower" == "n" ]]; then
   echo "아쉽군요. 리눅스는 강력하고 다양한 운영 체제입니다."
 else
   echo "유효하지 않은 입력입니다. 'yes' 또는 'no'를 입력하세요."
